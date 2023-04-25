07e574814ed1056f524879f2e951858245e7d9c6
Himan-Miku <himanshu28.mj@gmail.com>
2023-04-25 18:34:13 +0530

    network image

--- lib/learn_flutter_page.dart
+++ lib/learn_flutter_page.dart
@@ -23,83 +23,88 @@ class _LearnFlutterPageState extends State<LearnFlutterPage> {
           icon: const Icon(Icons.arrow_back_ios_new),
         ),
       ),
-      body: Column(
-        children: [
-          Image.asset('images/Nino.png'),
-          const SizedBox(
-            height: 10,
-          ),
-          const Divider(
-            color: Colors.blue,
-            thickness: 3,
-            indent: 50,
-            endIndent: 50,
-          ),
-          Container(
-            width: double.infinity,
-            color: Colors.lightBlue,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.only(top: 10.0, bottom: 20.0),
-            child: const Center(
-              child: Text(
-                'This is inside a container',
-                style: TextStyle(color: Colors.white),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('images/Nino.png'),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(
+              color: Colors.blue,
+              thickness: 3,
+              indent: 50,
+              endIndent: 50,
+            ),
+            Container(
+              width: double.infinity,
+              color: Colors.lightBlue,
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(top: 10.0, bottom: 20.0),
+              child: const Center(
+                child: Text(
+                  'This is inside a container',
+                  style: TextStyle(color: Colors.white),
+                ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: isSwitch ? Colors.green : Colors.blue,
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () => {debugPrint('ElevatedButton Button clicked')},
+              child: const Text('ElevatedButton Button'),
             ),
-            onPressed: () => {debugPrint('ElevatedButton Button clicked')},
-            child: const Text('ElevatedButton Button'),
-          ),
-          OutlinedButton(
-            onPressed: () => {debugPrint('Outlined Button clicked')},
-            child: const Text('Outlined Button'),
-          ),
-          TextButton(
-            onPressed: () => {debugPrint('Text Button clicked')},
-            child: const Text('Text Button'),
-          ),
-          GestureDetector(
-            onTap: () => {
-              debugPrint('Hello There'),
-            },
-            // behavior: HitTestBehavior.opaque,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-                Text('Row Text Widget'),
-                Icon(
-                  Icons.local_fire_department,
-                  color: Colors.blue,
-                ),
-              ],
+            OutlinedButton(
+              onPressed: () => {debugPrint('Outlined Button clicked')},
+              child: const Text('Outlined Button'),
             ),
-          ),
-          Switch(
-            value: isSwitch,
-            activeColor: Colors.green,
-            onChanged: (bool newValue) {
-              setState(() {
-                isSwitch = newValue;
-              });
-            },
-          ),
-          Checkbox(
-            value: isCheckbox,
-            onChanged: (bool? newValue) {
-              setState(() {
-                isCheckbox = newValue;
-              });
-            },
-          )
-        ],
+            TextButton(
+              onPressed: () => {debugPrint('Text Button clicked')},
+              child: const Text('Text Button'),
+            ),
+            GestureDetector(
+              onTap: () => {
+                debugPrint('Hello There'),
+              },
+              // behavior: HitTestBehavior.opaque,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text('Row Text Widget'),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+              value: isSwitch,
+              activeColor: Colors.green,
+              onChanged: (bool newValue) {
+                setState(() {
+                  isSwitch = newValue;
+                });
+              },
+            ),
+            Checkbox(
+              value: isCheckbox,
+              onChanged: (bool? newValue) {
+                setState(() {
+                  isCheckbox = newValue;
+                });
+              },
+            ),
+            Image.network(
+              'https://images6.alphacoders.com/991/thumb-1920-991135.jpg',
+            )
+          ],
+        ),
       ),
     );
   }
